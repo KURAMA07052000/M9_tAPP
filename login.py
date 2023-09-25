4d510254100882e23c9be8b66cfb38f9d7aa6e90
KURAMA07052000 <kevin.urama@gmail.com>
2023-09-25 21:14:11 +0100

    login stuff

---
+++ login.py
@@ -0,0 +1,25 @@
+from tkinter import *
+from tkinter import messagebox
+
+root=Tk()
+root.title('Login')
+root.geometry('925x500+300+200')
+root.configure(bg='white')
+root.resizable(False,False)
+
+img = PhotoImage(file='carrent.png')
+Label(root,image=img, bg='white').place(x=50,y=50)
+
+frame=Frame(root,width=350,height=350,bg='red')
+frame.place(x=480, y=70)
+
+heading=Label(frame,text="LOG IN", fg="#F08080", bg="white", font=("Microsft YaHei UI Light",23,"bold"))
+heading.place(x=100, y=5)
+
+user = Entry(frame, width=25, fg="black", border=0, bg="white", font=("Microsft YaHei UI Light",11))
+user.place(x=30, y=80)
+user.insert(0,"Username")
+
+Frame(frame, width=295, height=2, bg="black").place(x=25,y=107)
+
+root.mainloop()
