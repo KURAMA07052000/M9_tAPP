64e133f776e1945584b27eacbfa6cfbdc440c412
Harsh Raj Verma (student) <2903432V@student.gla.ac.uk>
2023-10-05 14:17:17 +0100

    added assets loader

--- controller/Controller.py
+++ controller/Controller.py
@@ -1,16 +1,33 @@
 from view.SignUp import SignUp
 from view.SignIn import SignIn
+from view.UserHome import UserHome
+from view.Wallet import Wallet
+
+from tkinter import PhotoImage
+import os
 
 class Controller():
     def __init__(self):
-        self.ALL_PAGES = [SignIn ,SignUp]
+        self.ALL_PAGES = [SignIn ,SignUp, UserHome, Wallet]
         self.VIEW = None
+
+    def init_assets(self):
+        self.ASSETS = {
+            'logo': PhotoImage(file=os.path.join(os.path.join(os.getcwd(),'assets'),'carrent1.png'))
+            }
 
     def setView(self,controller):
         self.VIEW = controller
+        self.init_assets()
 
     def toSignUp(self):
         self.VIEW.show_frame(SignUp)
 
     def toSignIn(self):
         self.VIEW.show_frame(SignIn)
+
+    def toUserHome(self):
+        self.VIEW.show_frame(UserHome)
+
+    def toWallet(self):
+        self.VIEW.show_frame(Wallet)
