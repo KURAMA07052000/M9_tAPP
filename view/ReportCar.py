37520b20f2450cd4567695512cbb7f518707e551
jhrluck <jhrluck@163.com>
2023-10-15 19:12:21 +0100

    Report a car 1

--- view/ReportCar.py
+++ view/ReportCar.py
@@ -1,54 +1,50 @@
 import tkinter as tk
+from tkinter import ttk
+from tkinter import *
 import tkinter.font as tkFont
 
 class ReportCar(tk.Frame):
-    def __init__(self, root):
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        self.CONTROLLER = controller
         #setting title
-        root.title("undefined")
-        #setting window size
-        width=600
-        height=500
-        screenwidth = root.winfo_screenwidth()
-        screenheight = root.winfo_screenheight()
-        alignstr = '%dx%d+%d+%d' % (width, height, (screenwidth - width) / 2, (screenheight - height) / 2)
-        root.geometry(alignstr)
-        root.resizable(width=False, height=False)
+        root.title("Report a car")
+        frame = Frame(self, width=350, height=650, bg='white')
 
-        GButton_485=tk.Button(root)
+        GButton_485 = tk.Button(root)
         GButton_485["bg"] = "#efefef"
-        ft = tkFont.Font(family='Times',size=10)
+        ft = tkFont.Font(family='Times', size=11)
         GButton_485["font"] = ft
         GButton_485["fg"] = "#000000"
         GButton_485["justify"] = "center"
         GButton_485["text"] = "CANCEL"
-        GButton_485.place(x=170,y=350,width=77,height=35)
+        GButton_485.place(x=290, y=370, width=131, height=36)
         GButton_485["command"] = self.GButton_485_command
 
-        GButton_901=tk.Button(root)
+        GButton_901 = tk.Button(root)
         GButton_901["bg"] = "#efefef"
-        ft = tkFont.Font(family='Times',size=10)
+        ft = tkFont.Font(family='Times', size=11)
         GButton_901["font"] = ft
         GButton_901["fg"] = "#000000"
         GButton_901["justify"] = "center"
         GButton_901["text"] = "CONFIRM"
-        GButton_901.place(x=310,y=350,width=77,height=35)
+        GButton_901.place(x=500, y=370, width=132, height=36)
         GButton_901["command"] = self.GButton_901_command
 
-        GLabel_77=tk.Label(root)
-        ft = tkFont.Font(family='Times',size=10)
-        GLabel_77["font"] = ft
-        GLabel_77["fg"] = "#333333"
-        GLabel_77["justify"] = "center"
-        GLabel_77["text"] = "Time for drop-off :"
-        GLabel_77.place(x=290,y=140,width=131,height=37)
+        GLineEdit_821 = tk.Entry(root)
+        GLineEdit_821["borderwidth"] = "1px"
+        ft = tkFont.Font(family='Times', size=10)
+        GLineEdit_821["font"] = ft
+        GLineEdit_821["fg"] = "#333333"
+        GLineEdit_821["justify"] = "center"
+        GLineEdit_821["text"] = "Type damage report here....."
+        GLineEdit_821.place(x=510, y=80, width=369, height=234)
+        GLineEdit_821["show"] = "Type damage report here....."
 
-        GListBox_636=tk.Listbox(root)
-        GListBox_636["borderwidth"] = "1px"
-        ft = tkFont.Font(family='Times',size=10)
-        GListBox_636["font"] = ft
-        GListBox_636["fg"] = "#333333"
-        GListBox_636["justify"] = "center"
-        GListBox_636.place(x=300,y=180,width=174,height=30)
+
+        self.drop_off_loc = ttk.Combobox(self, values=["Vehicle 1", "Vehicle 2", "Vehicle 3"], style='Red.TCombobox', justify='center')
+        self.drop_off_loc.place(x=120, y=80, width=260, height=30)
+        self.drop_off_loc.set("Chose from your ordered vehicles")
 
     def GButton_485_command(self):
         print("command")
@@ -57,7 +53,26 @@ class ReportCar(tk.Frame):
     def GButton_901_command(self):
         print("command")
 
-if __name__ == "__main__":
+
+
+if __name__ == '__main__':
     root = tk.Tk()
-    reportCar = ReportCar(root)
+    root.geometry('925x500+300+200')
+    root.configure(bg='white')
+    root.resizable(False, False)
+    root.title('ReportCar')
+
+    container = tk.Frame(root)
+    container.pack(side="top", fill="both", expand=True)
+    container.grid_rowconfigure(0, weight=1)
+    container.grid_columnconfigure(0, weight=1)
+
+    controller = None
+
+    pg = ReportCar(container, controller)
+    pg.grid(row=0, column=0, sticky="nsew")
+
     root.mainloop()
+
+
+
