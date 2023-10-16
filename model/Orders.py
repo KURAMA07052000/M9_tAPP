2dabef1390499fec3069caa1490a58e5ff718fcf
Elvis LIN <lzh000307@126.com>
2023-10-16 22:35:26 +0100

    order history

--- model/Orders.py
+++ model/Orders.py
@@ -10,6 +10,18 @@ import uuid
 
 
 class Orders:
+
+    def set_value(self, order_id: int, vehicle_id: int, user_id: int, end_time: datetime, start_time: datetime, pickup_location: str, dropoff_location: str, charge: float, damage_id: int):
+        self.order_id = order_id
+        self.vehicle_id = vehicle_id
+        self.user_id = user_id
+        self.end_time = end_time
+        self.start_time = start_time
+        self.pickup_location = pickup_location
+        self.dropoff_location = dropoff_location
+        self.charge = charge
+        self.damage_id = damage_id
+
     def __init__(self):
         pass
 
@@ -36,7 +48,40 @@ class Orders:
                 );''')
         con.commit()
 
-    def create_new(self):
+
+    def creat_new(self, order_id: int, vehicle_id: int, user_id: int, end_time: datetime, start_time: datetime, pickup_location: str, dropoff_location: str, charge: float, damage_id: int):
+        '''
+        Create a new order
+        '''
+        con, cur = [conn(), curr()]
+        order_id = str(uuid.uuid4())
+
+        cur.execute("""INSERT INTO Orders (order_id, vehicle_id, user_id, end_time, start_time,
+        pickup_location, dropoff_location, charge, damage_id) values (
+        ?,
+        ?,
+        ?,
+        ?,
+        ?,
+        ?,
+        ?,
+        ?,
+        ?
+        );""", [order_id, vehicle_id, user_id, end_time, start_time, pickup_location, dropoff_location, charge, damage_id])
+        con.commit()
+
+
+    def order_history_user(self, user_id: int):
+        '''
+        Get order history for a user
+        '''
+        con, cur = [conn(), curr()]
+
+        cur.execute("""SELECT * FROM Orders WHERE user_id = ?;""", [user_id])
+        row = cur.fetchall()
+        return row
+
+    def create_new_test(self):
         '''
         create vehicle_id
         '''
@@ -56,14 +101,14 @@ class Orders:
         pickup_location, dropoff_location, charge, damage_id) values (
         ?, 
         ?, 
-        ?, 
+        'd830eb0f-9b9f-4b0d-8d4f-01175ec9300e', 
         null, 
         ?, 
         'University of Glasgow',
         'Glasgow Queen street', 
         99.12,
         null
-        );""", [order_id, vehicle_id, user_id, st])
+        );""", [order_id, vehicle_id, st])
 
         con.commit()
 
