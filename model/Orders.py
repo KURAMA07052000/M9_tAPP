69351276130c25b254fa6fc1021bccdcc3548f2f
Elvis LIN <lzh000307@126.com>
2023-10-19 02:20:18 +0100

    add method to orders model

--- model/Orders.py
+++ model/Orders.py
@@ -22,20 +22,21 @@ class Orders:
         self.charge = charge
         self.damage_id = damage_id
 
+
     def __init__(self):
+        self.con = conn()
+        self.cur = curr()
         pass
 
     def CREATE_TABLE(self, saftey='on'):
         '''
         Create a vehicle table
         '''
-        con = conn()
-        cur = curr()
 
         if saftey == 'off':
-            cur.execute('''DROP TABLE IF EXISTS Orders''')
+            self.cur.execute('''DROP TABLE IF EXISTS Orders''')
 
-        cur.execute('''CREATE TABLE IF NOT EXISTS Orders (
+        self.cur.execute('''CREATE TABLE IF NOT EXISTS Orders (
                 order_id text PRIMARY KEY, 
                 vehicle_id text not null,
                 user_id text not null,
@@ -46,17 +47,16 @@ class Orders:
                 charge double(10,2),
                 damage_id text
                 );''')
-        con.commit()
+        self.con.commit()
 
 
     def creat_new(self, order_id: int, vehicle_id: int, user_id: int, end_time: datetime, start_time: datetime, pickup_location: str, dropoff_location: str, charge: float, damage_id: int):
         '''
         Create a new order
         '''
-        con, cur = [conn(), curr()]
         order_id = str(uuid.uuid4())
 
-        cur.execute("""INSERT INTO Orders (order_id, vehicle_id, user_id, end_time, start_time,
+        self.cur.execute("""INSERT INTO Orders (order_id, vehicle_id, user_id, end_time, start_time,
         pickup_location, dropoff_location, charge, damage_id) values (
         ?,
         ?,
@@ -68,24 +68,19 @@ class Orders:
         ?,
         ?
         );""", [order_id, vehicle_id, user_id, end_time, start_time, pickup_location, dropoff_location, charge, damage_id])
-        con.commit()
+        self.con.commit()
 
 
     def order_history_user(self, user_id: int):
-        '''
-        Get order history for a user
-        '''
-        con, cur = [conn(), curr()]
 
-        cur.execute("""SELECT * FROM Orders WHERE user_id = ?;""", [user_id])
-        row = cur.fetchall()
+        self.cur.execute("""SELECT * FROM Orders WHERE user_id = ?;""", [user_id])
+        row = self.cur.fetchall()
         return row
 
     def create_new_test(self):
         '''
         create vehicle_id
         '''
-        con, cur = [conn(), curr()]
 
         order_id = uuid.uuid4()
         vehicle_id = str(uuid.uuid4())
@@ -97,7 +92,7 @@ class Orders:
         # w = Wallet()
         # w.create_new(wallet_id, user_id)
 
-        cur.execute("""INSERT INTO Orders (order_id, vehicle_id, user_id, end_time, start_time, 
+        self.cur.execute("""INSERT INTO Orders (order_id, vehicle_id, user_id, end_time, start_time, 
         pickup_location, dropoff_location, charge, damage_id) values (
         ?, 
         ?, 
@@ -110,7 +105,41 @@ class Orders:
         null
         );""", [order_id, vehicle_id, st])
 
-        con.commit()
+        self.con.commit()
+
+    '''
+        method: create_new_order
+        input: vehicle_id, user_id, start_time, end_time, pickup_location, dropoff_location
+        Figure out if we need to enter start time
+        return: order_id
+    '''
+    def create_new_order(self, vehicle_id: str, user_id: str, start_time: datetime, end_time: datetime, pickup_location: str, dropoff_location: str):
+        order_id = str(uuid.uuid4())
+        self.cur.execute("""INSERT INTO Orders (order_id, vehicle_id, user_id, end_time, start_time,
+        pickup_location, dropoff_location, charge, damage_id) values (
+        ?,
+        ?,
+        ?,
+        ?,
+        ?,
+        ?,
+        ?,
+        -1, 
+        ?
+        );""", [order_id, vehicle_id, user_id, end_time, start_time, pickup_location, dropoff_location, None])
+        self.con.commit()
+        # charge = -1 means the order is not completed
+        # charge = -2 means the order is completed but not paid
+        return order_id
+
+    '''
+        method: complete_order
+        input: order_id, dropoff_location = None, end_time = None, damage_id = None
+        return: order_id
+    '''
+    # TODO:
+    # def complete_order(self, order_id: str, dropoff_location: str = None, end_time: datetime = None, damage_id: str = None):
+        # get Order first
 
 
 if __name__ == '__main__':
